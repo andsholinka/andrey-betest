a311eb0449348abc0d8d3dbd3665944df3648deb
Andrey Sholinka <sholinka2111@gmail.com>
2024-04-18 14:14:43 +0700

    Validate authorization header JWT

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -10,6 +10,16 @@ const auth = async (req, res, next) => {
             status: res.statusCode,
             message: 'Unauthorized!'
         });
+
+        jwt.verify(token, process.env.SECRET_KEY, async function (err, decoded) {
+            if (err) return res.status(401).send({
+                status: res.statusCode,
+                message: 'Failed to authenticate token.'
+            });
+
+            res.locals.userId = decoded.data.id;
+            next();
+        });
     } catch (error) {
         res.status(500).json({
             message: error.message
